00f23b894a0f9d3c185f8c1302c1ff9478d86fd0
wigginz <wigginz@gmail.com>
2010-08-18 00:03:28 +0000

    added file open item to open a widget package (.wgt) directly without having to unzip it first.

--- emulator/extension/chrome/content/emulator/emulator.js
+++ emulator/extension/chrome/content/emulator/emulator.js
@@ -47,6 +47,7 @@ var jwe_emulator =
   eventsLoaded : false,
   currentPanel : null,
   extensions : null,
+  debugMode: null,
 
   init : function()
   {
@@ -398,6 +399,8 @@ var jwe_emulator =
     if ( (event != null) && (event.button != 0) )
       return;
     
+    this.debugMode = inTab;
+    
     if ( inTab )
     {
       this.emulator.emulateWidget(content.location.pathname, content.document.documentElement, null, false);
@@ -734,7 +737,83 @@ var jwe_emulator =
       var req = new XMLHttpRequest();
       req.open("GET", "file://"+fp.file.path, false); 
       req.send(null);
-      this.emulator.emulateWidget(fp.file.path, req.responseXML.documentElement, null, true);
+      this.emulator.emulateWidget(fp.file.path, req.responseXML.documentElement, null, this.debugMode);
+      this.reload();
+    }
+  },
+  
+  openWidgetPackage : function()
+  {
+    var nsIFilePicker = Components.interfaces.nsIFilePicker;
+    var fp = Components.classes["@mozilla.org/filepicker;1"].createInstance(nsIFilePicker);
+    fp.init(window, "Select a JIL Widget Package", nsIFilePicker.modeOpen);
+    fp.appendFilter("JIL Widget Package File (*.wgt)","*.wgt");
+    
+    var res = fp.show();
+    // accept an OK 
+    if ( res == nsIFilePicker.returnOK )
+    {
+      var zipReader = Components.classes["@mozilla.org/libjar/zip-reader;1"]
+                .createInstance(Components.interfaces.nsIZipReader);
+      zipReader.open(fp.file);
+      zipReader.test(null);      
+      TransitCommon.debug("Opening widget package from path "+fp.file.path);
+      
+      // create a temp directory
+      var tempDirectory = Components.classes["@mozilla.org/file/directory_service;1"].getService(Components.interfaces.nsIProperties).get("TmpD", Components.interfaces.nsIFile);
+      TransitCommon.debug("Local machine temp directory is "+tempDirectory.path);
+      
+      var wgtDir = Components.classes["@mozilla.org/file/local;1"].createInstance(Components.interfaces.nsILocalFile);
+      wgtDir.initWithPath(tempDirectory.path+TransitCommon.getFileSeparator()+fp.file.leafName);
+      TransitCommon.debug("Creating directory to unzip widget contents to: "+tempDirectory.path+TransitCommon.getFileSeparator()+fp.file.leafName);
+      
+      // if it exists, delete it
+      if ( wgtDir.exists() )
+      {
+        TransitCommon.debug("Directory already exists, removing it and re-creating it.");
+        wgtDir.remove(true);
+      } 
+      wgtDir.create(Components.interfaces.nsILocalFile.DIRECTORY_TYPE, 0777);
+      
+      entries = zipReader.findEntries(null);
+      var foundConfig = false;
+      while (entries.hasMore()) 
+      {
+        var fileName = entries.getNext();
+        if ( fileName.indexOf("config.xml") > -1 )
+          foundConfig = true;
+        
+        TransitCommon.debug("Extracting file: "+fileName+" to: "+wgtDir.path+TransitCommon.getFileSeparator()+fileName);
+        
+        var extractedFile = Components.classes["@mozilla.org/file/local;1"].createInstance(Components.interfaces.nsILocalFile);
+        extractedFile.initWithPath(wgtDir.path+TransitCommon.getFileSeparator()+fileName);
+        
+        if ( extractedFile.exists() )
+          continue;     
+        
+        try 
+        {
+          extractedFile.create(Components.interfaces.nsILocalFile.DIRECTORY_TYPE, 0777);
+        }
+        catch (e)
+        {
+          TransitCommon.alert("extractExtensionsFiles: failed to create target file for extraction " +
+                " file = " + extractedFile.path + ", exception = " + e + "\n");
+        }        
+        zipReader.extract(fileName, extractedFile);
+      }
+      zipReader.close();
+      
+      if ( !foundConfig )
+        TransitCommon.alert("Widget package does not appear to be a valid widget package, no config.xml file found within package.");
+      else
+      {
+        var req = new XMLHttpRequest();
+        req.open("GET", "file://"+wgtDir.path+TransitCommon.getFileSeparator()+"config.xml", false); 
+        req.send(null);
+        this.emulator.emulateWidget(wgtDir.path+TransitCommon.getFileSeparator()+"config.xml", req.responseXML.documentElement, null, this.debugMode);
+        this.reload();
+      }
     }
   },
   
